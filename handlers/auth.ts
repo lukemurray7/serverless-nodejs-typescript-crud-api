21083db5b2e8d387af16fb6b8dd79a7b9214a387
lukemurray7 <lukemurray737@googlemail.com>
2019-12-20 17:04:41 +0000

    updated auth function to check token against api key stored in ssms

--- handlers/auth.ts
+++ handlers/auth.ts
@@ -1,22 +1,41 @@
 import { SSM } from 'aws-sdk';
+import { CustomAuthorizerResult, CustomAuthorizerEvent } from 'aws-lambda';
 
-const getAPIKey = async () => {
-  const ssm = new SSM();
-  console.log(ssm);
-  const key = await ssm.getParameter({ Name: 'test' }).promise();
-  return key;
+const generatePolicy = (authenticated: boolean, methodArn: string): CustomAuthorizerResult => {
+  return {
+    // TODO work out what principal id is used for.
+    principalId: '12345',
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: authenticated ? 'Allow' : 'Deny',
+          Resource: methodArn,
+        }
+      ]
+    }
+  };
 };
 
-const handler = async (event) => {
-  const token = event.authorizationToken.toLowerCase();
-  console.log(token);
-  try {
-    const key = await getAPIKey();
-    console.log(token, key);
-  } catch (error) {
-    console.log(error);
-  }
+const getAPIKey = async (): Promise<string> => {
+  const ssm: SSM = new SSM();
+  const result = await ssm.getParameter({ Name: process.env.SECRET_NAME }).promise();
+  return result.Parameter.Value;
+};
 
+const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
+  const token: string = event.authorizationToken.toLowerCase();
+  console.log(event.methodArn);
+  try {
+    const key: string = await getAPIKey();
+    if (token === key) {
+      return generatePolicy(true, event.methodArn);
+    }
+    throw new Error("Unauthenticated");
+  } catch (error) {
+    return generatePolicy(false, event.methodArn);
+  }
 }
 
 
