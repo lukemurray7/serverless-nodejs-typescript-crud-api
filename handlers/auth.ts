b1292b0ea1b06535cbfc0a3bcf34318b30cb5710
lukemurray7 <lukemurray737@googlemail.com>
2019-12-23 18:37:25 +0000

    updated ts config, auth handler and serverless file for lamdas to use auth function

--- handlers/auth.ts
+++ handlers/auth.ts
@@ -1,6 +1,8 @@
 import { SSM } from 'aws-sdk';
 import { CustomAuthorizerResult, CustomAuthorizerEvent } from 'aws-lambda';
 
+const secretName = process.env.SECRET_NAME || '';
+
 const generatePolicy = (authenticated: boolean, methodArn: string): CustomAuthorizerResult => {
   return {
     // TODO work out what principal id is used for.
@@ -20,14 +22,19 @@ const generatePolicy = (authenticated: boolean, methodArn: string): CustomAuthor
 
 const getAPIKey = async (): Promise<string> => {
   const ssm: SSM = new SSM();
-  const result = await ssm.getParameter({ Name: process.env.SECRET_NAME }).promise();
-  return result.Parameter.Value;
+  const result = await ssm.getParameter({ Name: secretName }).promise();
+  if (result.Parameter && result.Parameter.Value) {
+    return result.Parameter.Value;
+  }
+  throw new Error("Could not find API Key");
 };
 
 const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
-  const token: string = event.authorizationToken.toLowerCase();
-  console.log(event.methodArn);
   try {
+    const token: string = event.authorizationToken ? event.authorizationToken.toLowerCase() : '';
+    if (!token) {
+      throw new Error("Unauthorized");
+    }
     const key: string = await getAPIKey();
     if (token === key) {
       return generatePolicy(true, event.methodArn);
